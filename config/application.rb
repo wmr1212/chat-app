3d6d014a40bf961a2956bd783a79225e643e9444
wmr1212 <s21260034@nucba.ac.jp>
2021-11-16 00:49:23 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module ChatApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    end
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
